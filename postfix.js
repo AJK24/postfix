10da6a6bd89442e19d9b6bece502aff200c81c2d
Andrew <akv9509@saintjoe.edu>
2015-02-02 15:04:41 -0500

    This file makes the postfix printed in reverse order with an undefined added

--- postfix.js
+++ postfix.js
@@ -43,39 +43,104 @@ function length(){
 }
 
 
+
+
+
+function doMath(variable, first, second){
+	var equation = ''; //going to be returned as string
+
+	if(variable == '*'){
+		(first * second);
+	}
+	else if(variable == '/'){
+		(first / second);
+
+	}
+	else if(variable == '+'){
+		(first + second);
+	}
+	else if(variable == '-'){
+		(first - second);
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
 function convertToPost(equation){
 	var final = new Stack();
-	var temp = new Stack();
+	var operators = new Stack();
+//	var first = 0;
+//	var second = 0;
+
 
 	for(var i = 0; i < equation.length; i++){
 //console.log('I am pushing something '+ equation[i]);		
 	
 		//pushes to the final stack
-		if(equation[i] >= '0' || equation[i] <= '9'){
+		if(equation[i] >= '0' &&  equation[i] <= '9'){
 			final.push(equation[i]);
-		}
+					}
 		//pushes to the temp stack
 		else if(equation[i] == '+' || equation[i] == '-' || equation == '/' || equation[i] == '*'){
-			temp.push(equation[i]);
+			// push operator to operator stack
+			operators.push(equation[i]);
+			//add the next number in the equation to the final stack
+			final.push(equation[i+1]);
+			//add the operator from the operator stack to the final stack
+			final.push(operators.pop());
+			//must increment up one to count make the loop work properly since the next iteration was just pushed to final.
+			i++;
+
 		}
 		//if invalid character will print
 		else
-			console.log("Erroe - invalid number");
+			console.log("Error - invalid number");
 	}
+
+//peeking to the top of final should be -
+//console.log(final.peek());
 
 //testing should not be zero
-console.log('length of temp ' + temp.length());
-console.log('length of final ' + final.length());
+//console.log('length of temp ' + operators.length());
+//console.log('length of final ' + final.length());
 
-	///TESTING SHOULD PRINT CONTENTS OF TEMP and should empty temp
+
+//testing string to be printed
+var postequ
+	///TESTING SHOULD PRINT CONTENTS OF THE FINAL POST EQUATION
 	while(final.length() > 0 ){
-		console.log(final.pop());	
+		postequ += final.pop();
 	}
-
+	console.log(postequ);
+//	console.log("just checked final now printing operators which should be empty");
+//	while(operators.length() > 0){
+///		console.log(operators.pop());
+//	}
 //testing should print out the stack,not contents
-console.log("I am peeking " + temp.peek()); 
+//console.log("I am peeking " + temp.peek()); 
 }
 
 
-convertToPost("1+23-15");
+
+
+
+
+
+
+
+
+
+
+
+
+convertToPost("1+2-5");
 
